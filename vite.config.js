7cad88ec38f255fd5127809e4b67a11d5bcad555
nicofrezzia <frezzia1629@gmail.com>
2025-02-17 15:41:13 -0700

    Fixed routing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'Movies', // Set the correct GitHub Pages path
   define: {
-    'import.meta.env.VITE_TMDB_API_KEY': JSON.stringify('4f58c0dcb776ef654fc0eaedf64c2949'),
+    'import.meta.env.VITE_TMDB_API_KEY': JSON.stringify('YOUR_TMDB_API_KEY_HERE'),
   },
 });
