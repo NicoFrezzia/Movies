de096fe47cb1feaeaab75454692b6793b7bde3b6
nicofrezzia <frezzia1629@gmail.com>
2025-02-17 15:36:30 -0700

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_TMDB_API_KEY': JSON.stringify('4f58c0dcb776ef654fc0eaedf64c2949'),
+  },
+});
