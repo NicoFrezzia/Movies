cc21bb9a8f4726b9fc9dd61b991a3ae42704f35f
nicofrezzia <frezzia1629@gmail.com>
2025-02-18 20:33:11 -0700

    Fix?

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Now GitHub Pages will serve from the main branch root
+  base: "/Movies/", // Now GitHub Pages will serve from the main branch root
 });
